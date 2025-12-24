9750d9c54c8f90fd2b86708acb8ea799e6a3ab84
personal <mahmoddabdalkareem2@gmail.com>
2025-12-24 13:34:40 +0300

    change4

--- jcript.js
+++ jcript.js
@@ -1,12 +1,10 @@
 const darkModeBtn = document.getElementById('darkModeBtn');
 
 darkModeBtn.addEventListener('click', () => {
-    document.body.classList.toggle('dark-theme');
-    
-    // تغيير نص الزر بناءً على الوضع
-    if (document.body.classList.contains('dark-theme')) {
-        darkModeBtn.textContent = 'الوضع الفاتح';
-    } else {
-        darkModeBtn.textContent = 'الوضع الداكن';
-    }
+  document.body.classList.toggle('dark-theme');
+
+  darkModeBtn.textContent =
+    document.body.classList.contains('dark-theme')
+      ? 'الوضع الفاتح'
+      : 'الوضع الداكن';
 });
